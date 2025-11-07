5453683c1b917192cb57cf7356c0c5bcbd4960eb
GBENCHEIKH <ghita.bencheikh@gmail.com>
2025-11-07 10:37:19 +0100

    Ajout du fichier fraction.cpp

---
+++ boucle_1/partie_2/fraction.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+You can write your class Fraction here
+
+*/
+
+int main(){
+    /* Test constructors */
+
+    Fraction f0;
+
+    if (f0.getNumerateur() == 1 && f0.getDenominateur() == 1) {
+        cout << "[OK] Default constructor works" << endl;
+    }
+    else {
+        cout << "[FAILURE] Default constructor doesn't work" << endl;
+    }
+
+    Fraction f1(1, 2);
+
+    if (f1.getNumerateur() == 1 && f1.getDenominateur() == 2) {
+        cout << "[OK] Constructor with parameters works" << endl;
+    }
+    else {
+        cout << "[FAILURE] Constructor with parameters doesn't work" << endl;
+    }
+
+    /* Test add */ 
+
+    Fraction f2(3, 4);
+    Fraction f3 = f1.add(f2);
+
+    if (f3.getNumerateur() == 10 && f3.getDenominateur() == 8) {
+        cout << "[OK] Add method works" << endl;
+    }
+    else {
+        cout << "[FAILURE] Add method doesn't work" << endl;
+    }
+
+    /* Test minus */
+
+    f3 = f1.minus(f2);
+
+    if (f3.getNumerateur() == -2 && f3.getDenominateur() == 8) {
+        cout << "[OK] Minus method works" << endl;
+    }
+    else {
+        cout << "[FAILURE] Minus method doesn't work" << endl;
+    }
+
+    /* Test multiply */
+
+    f3 = f1.multiply(f2);
+
+    if (f3.getNumerateur() == 3 && f3.getDenominateur() == 8) {
+        cout << "[OK] Multiply method works" << endl;
+    }
+    else {
+        cout << "[FAILURE] Multiply method doesn't work" << endl;
+    }
+
+    /* Test divide */
+
+    f3 = f1.divide(f2);
+
+    if (f3.getNumerateur() == 4 && f3.getDenominateur() == 6) {
+        cout << "[OK] Divide method works" << endl;
+    }
+    else {
+        cout << "[FAILURE] Divide method doesn't work" << endl;
+    }
+
+    return 0;
+}
