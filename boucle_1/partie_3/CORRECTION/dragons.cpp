a7ac7380d6a7bcc6fd6087180a3ebf94da109dd8
GBENCHEIKH <ghita.bencheikh@gmail.com>
2025-11-07 16:24:31 +0100

    Ajout de la correction partie_3

---
+++ boucle_1/partie_3/CORRECTION/dragons.cpp
@@ -0,0 +1,90 @@
+#include <iostream> 
+
+using namespace std; 
+
+class Creature{
+private: 
+    int pv; 
+    int pa; 
+    int niveau; 
+
+public : 
+    Creature(): pv(0), pa (0), niveau (0){}
+    Creature(int pv, int pa, int niveau): pv(pv), pa(pa), niveau(niveau){}
+
+    void affiche(){
+        cout << "Creature" << endl; 
+        cout << "PV : " << pv << endl; 
+        cout << "PA : " << pa << endl; 
+        cout << "Niveau :"<< niveau << endl; 
+    }
+
+    int attaque(){
+        return pa; 
+    }
+
+    void subitDegat(int degats){
+        pv -= degats; 
+    }
+
+    bool estVivant(){
+        if(pv > 0)
+            return true; 
+        else 
+            return false; 
+    }
+};
+
+class Heros{
+private: 
+    string nom; 
+    int pv; 
+    int pa; 
+
+public : 
+    // constructeurs
+    Heros(): nom(""), pv(0), pa(0){}
+    Heros(string nom, int pv, int pa): nom(nom), pv(pv), pa(pa){}
+
+    void affiche(){
+        cout << "Heros"<< endl; 
+        cout << "Nom :"<< nom << endl;
+        cout << "PV : " << pv << endl; 
+        cout << "PA : " << pa << endl; 
+    }
+
+    int attaque(){
+        return pa; 
+    }
+
+    void subitDegat(int degats){
+        pv -= degats; 
+    }
+
+    bool estVivant(){
+        if(pv > 0)
+            return true; 
+        else 
+            return false; 
+    }
+}; 
+
+
+int main(){
+    Heros H = Heros("Hrun", 10, 8); 
+    Creature C = Creature(10,10,5); 
+
+    H.affiche(); 
+    C.affiche(); 
+
+    int d = H.attaque(); 
+
+    C.subitDegat(d); 
+
+    d = C.attaque(); 
+
+    H.subitDegat(d); 
+
+    H.affiche(); 
+    C.affiche(); 
+}
