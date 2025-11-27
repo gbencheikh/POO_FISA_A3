b34644808c4887fbd60b47aacc12778e6d199e49
GBENCHEIKH <ghita.bencheikh@gmail.com>
2025-11-27 13:58:02 +0100

    Ajout du template

--- boucle_5/exercice_6/main2.cpp
+++ boucle_5/exercice_6/main2.cpp
@@ -1,83 +1,95 @@
-#include <iostream> 
-using namespace std; 
-
-typedef double elementType; 
+#include <iostream>
+using namespace std;
 
 template<typename T>
-class Element{
-private: 
-    T val; 
+class Element {
+private:
+    T val;
 
-public :
-    Element():val(0){}
-    
-    // getters 
-    T getValue(){return val; }
-
-    // setters
-    void setValue(T value){val=value;}
+public:
+    Element() : val() {}
+    T getValue() { return val; }
+    void setValue(T value) { val = value; }
 };
 
 template<typename T>
-class Liste{
-private : 
-    T** elements; 
-    int n; 
+class Liste {
+private:
+    Element<T>** elements;  
+    int n;
 
-public : 
-    Liste(){
-        n = 0; 
-        elements = new Element*[n]; 
-    }
+public:
+    Liste() : n(0), elements(nullptr) {}
 
-    void ajouter(T value){
-        n++; 
-        Element** T = new Element*[n]; 
+    void ajouter(T value) {
+        // Nouveau tableau plus grand
+        Element<T>** temp = new Element<T>*[n + 1];
 
-        Element* elem = new Element;
-        elem->setValue(value);  
+        // Nouveau premier élément
+        temp[0] = new Element<T>();
+        temp[0]->setValue(value);
 
-        T[0] = elem;
-        
-        for(int i = 0; i < n-1; i++){
-            T[i+1] = elements[i]; 
+        // Copier anciens éléments
+        for (int i = 0; i < n; i++) {
+            temp[i + 1] = elements[i];
         }
 
-        elements = T; 
+        // Libérer ancien tableau (mais pas les Element<T>)
+        delete[] elements;
+
+        elements = temp;
+        n++;
     }
 
-    void ajouter(T value, int i){
-        Element* elem = new Element;
+    void ajouter(T value, int i) {
+        if (i < 0 || i > n) return;
+
+        Element<T>** temp = new Element<T>*[n + 1];
+        Element<T>* elem = new Element<T>();
         elem->setValue(value);
 
-        n++; 
-        Element** T = new Element*[n]; 
+        for (int j = 0; j < i; j++)
+            temp[j] = elements[j];
 
-        for(int j = 0; j < i; j++){
-            T[j] = elements[j]; 
-        }
-        T[i] = elem; 
-        for(int j = i+1; j < n; j++){
-            T[j] = elements[j-1]; 
-        }
+        temp[i] = elem;
 
-        elements = T; 
+        for (int j = i; j < n; j++)
+            temp[j + 1] = elements[j];
+
+        delete[] elements;
+
+        elements = temp;
+        n++;
     }
 
-    void affiche(){
-        for(int i = 0 ; i < n ; i++){
-            cout << elements[i]->getValue() << endl; 
+    void affiche() {
+        for (int i = 0; i < n; i++) {
+            cout << elements[i]->getValue() << endl;
         }
+    }
+
+    ~Liste() {
+        for (int i = 0; i < n; i++)
+            delete elements[i];
+        delete[] elements;
     }
 };
 
-int main(){
-    Liste<int>* l = new Liste<int>(); 
-    l->ajouter(5); 
-    l->ajouter(4); 
+int main() {
+    Liste<int>* l = new Liste<int>();
+    l->ajouter(5);
+    l->ajouter(4);
     l->ajouter(3);
-    l->ajouter(9,1);  
-    l->affiche(); 
+    l->ajouter(9, 1);
+    l->affiche();
 
-    return 0; 
+    delete l;
+
+    Liste<string>* l2 = new Liste<string>();
+    l2->ajouter("Hello");
+    l2->ajouter("CPI");
+    l2->ajouter("A2");
+    l2->affiche();
+
+    return 0;
 }
